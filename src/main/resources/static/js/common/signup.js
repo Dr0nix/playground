026b5c7004c40b2fdb64003f4a763d7f4fec886f
mjjeon <mj.jeon@zettasoft.co,kr>
2025-09-16 13:41:25 +0900

    [fe-feat] 회원가입 > 화면 1. 화면 개발 2. 기입정보 유효성 검사 로직 개발

---
+++ src/main/resources/static/js/common/signup.js
@@ -0,0 +1,79 @@
+$(document).ready(() => {
+    setEventListner();
+});
+
+function gotoLoginPage() {
+    console.log('go back!!')
+    location.href = '/';
+}
+
+function submit() {
+
+}
+
+function validateEveryThing() {
+    var isValidName = validateName();
+    var isValidEmail = validateEmail();
+    var isValidPw = validatePassword();
+
+    if(isValidName && isValidEmail && isValidPw) {
+        $('#signupSubmit').attr('disabled', false);
+    }
+    else {
+        $('#signupSubmit').attr('disabled', true);
+    }
+}
+
+function validateName() {
+    var result = false;
+
+    var name = $('#su-name').val();
+
+    // null이 아니고 길이가 2 이상이라면
+    if(name !== null && name.length > 1) {
+        result = true;
+    }
+
+    return result;
+}
+
+function validateEmail() {
+    var result = false;
+
+    var email = $('#su-email').val();
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // 이메일 패턴 정규식
+
+    // null이 아니고 이메일 패턴이 맞다면
+    if(email !== null && emailPattern.test(email)) {
+        result = true;
+    }
+
+    return result;
+}
+
+function validatePassword() {
+    var result = false;
+
+    var pw1 = $('#su-password').val();
+    var pw2 = $('#su-password2').val();
+
+    var pwPattern = /^(?=.*[A-Z])(?=.*\d).{8,}$/; // 대문자, 숫자 포함 8자리 이상
+
+    // null이 아니고 pw패턴에 맞으며 입력과 확인 값이 같으면
+    if(pw1 !== null && pwPattern.test(pw1) && pw1 === pw2) {
+        result = true;
+    }
+
+    return result;
+}
+
+function setEventListner() {
+    $('#back').click(gotoLoginPage);
+
+    $('#signupSubmit').click(submit);
+
+    $('#su-name').on('change', validateEveryThing);
+    $('#su-email').on('change', validateEveryThing);
+    $('#su-password').on('change', validateEveryThing);
+    $('#su-password2').on('change', validateEveryThing);
+}
