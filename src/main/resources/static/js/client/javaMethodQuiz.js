59a87c8c05075b3704c0fa2349eca8c72cd295a4
mjjeon <mj.jeon@zettasoft.co,kr>
2025-09-23 15:38:41 +0900

    [fe-feat] 퀴즈 > 자바메서드 1. 화면 개발

--- src/main/resources/static/js/client/javaMethodQuiz.js
+++ src/main/resources/static/js/client/javaMethodQuiz.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
-
+    setEventListener();
 });
+
+function setEventListener() {
+    $('#startBtn').click(() => {
+        console.log('good')
+        $('#introStage').addClass('hidden');
+        $('#quizStage').removeClass('hidden');
+    })
+}
