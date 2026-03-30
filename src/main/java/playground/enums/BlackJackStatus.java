b5267ace31314323540c454924bab2252b4f9ebf
mjjeon <mj.jeon@zettasoft.co,kr>
2026-03-30 17:52:23 +0900

    [feat] 블랙잭 1. 기능 개발중

--- src/main/java/playground/enums/BlackJackStatus.java
+++ src/main/java/playground/enums/BlackJackStatus.java
@@ -4,7 +4,5 @@ public enum BlackJackStatus {
     READY,          // 게임 시작 전
     PLAYER_TURN,    // 플레이어 턴
     DEALER_TURN,    // 딜러 턴
-    PLAYER_BUST,    // 플레이어 버스트
-    DEALER_BUST,    // 딜러 버스트
     FINISHED        // 게임 종료
 }
